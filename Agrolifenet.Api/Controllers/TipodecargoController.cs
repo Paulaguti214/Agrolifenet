597e0f5dfc55883794ffde226c6a1ee11c253fbe
FAMROGU\Paula <juan_esteban_199501@hotmail.com>
2024-11-16 09:39:27 -0500

    Asegurar Codigo

---
+++ Agrolifenet.Api/Controllers/TipodecargoController.cs
@@ -0,0 +1,50 @@
+﻿using Agrolifenet.Dominio.Entidades;
+using Agrolifenet.Dominio.Servicios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agrolifenet.Api.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TipodecargoController : ControllerBase
+    {
+        private readonly ILogger<TipodecargoController> _logger;
+        private readonly ITipodecargoServicio _tipodecargoServicio;
+
+        public TipodecargoController(ILogger<TipodecargoController> logger, ITipodecargoServicio tipodecargoServicio)
+        {
+            _logger = logger;
+            _tipodecargoServicio = tipodecargoServicio;
+        }
+        [HttpPost("InsertarTiposdecargo")]
+        public async Task AgrgarTipodecargo(string tipodecargo,  bool estadoTiposdecargo)
+        {
+            await _tipodecargoServicio.Agregar(tipodecargo,  estadoTiposdecargo);
+        }
+        [HttpGet("ListarTiposdecargo")]
+        public async Task<IEnumerable<TiposdeCargo>> ListarTiposdecargo()
+        {
+            return await _tipodecargoServicio.ListarTiposdecargo();
+        }
+        [HttpGet("BuscarTiposdecargo")]
+        public async Task<TiposdeCargo> SeleccionarTiposdecargo(int idTiposdecargo)
+        {
+            return await _tipodecargoServicio.SeleccionarTiposdecargo(idTiposdecargo);
+        }
+        [HttpDelete("EliminarTiposdecargo")]
+        public async Task EliminarTiposdecargo(int idTiposdecargo)
+        {
+            await _tipodecargoServicio.EliminarTiposdecargo(idTiposdecargo);
+        }
+        [HttpPut("ActualizarTiposdecargo")]
+        public async Task ActualizarTiposdecargo(int idTiposdecargo, string tipodecargo, bool estadoTiposdecargo)
+        {
+            await _tipodecargoServicio.ActualizarTiposdecargo(idTiposdecargo, tipodecargo,  estadoTiposdecargo);
+        }
+
+
+
+
+    }
+}
--- Agrolifenet.Dominio/Servicios/ITipodecargoServicio.cs
+++ Agrolifenet.Dominio/Servicios/ITipodecargoServicio.cs
@@ -8,7 +8,10 @@ namespace Agrolifenet.Dominio.Servicios
 {
     public interface ITipodecargoServicio
     {
-        Task Agregar(string tipodecargo, DateTime fechadecreacionTiposdecargo, DateTime fechademodificacionTiposdecargo,
-            bool estadoTiposdecargo);
+        Task Agregar(string tipodecargo,  bool estadoTiposdecargo);
+        Task<IEnumerable<TiposdeCargo>> ListarTiposdecargo();
+        Task<TiposdeCargo> SeleccionarTiposdecargo(int idTiposdecargo);
+        Task EliminarTiposdecargo(int idTiposdecargo);
+        Task ActualizarTiposdecargo(int idTiposdecargo, string tipodecargo,bool estadoTiposdecargo);
     }
 }
