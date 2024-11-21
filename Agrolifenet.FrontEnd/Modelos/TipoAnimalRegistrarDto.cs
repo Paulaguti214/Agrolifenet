cf049b1d26d5378e5b3c6baf23219af7b5e4dc51
FAMROGU\Paula <juan_esteban_199501@hotmail.com>
2024-11-20 23:33:45 -0500

    asegurar codigo

---
+++ Agrolifenet.FrontEnd/Modelos/TipoAnimalRegistrarDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agrolifenet.FrontEnd.Modelos
+{
+    public class TipoAnimalRegistrarDto
+    {
+        [Required(ErrorMessage = "El Tipo de animal es obligatorio")]
+        public string TipoAnimal { get; set; } = string.Empty;
+        public bool Estado { get; set; }
+    }
+}
