309445843ef178a7abcf75ffb4ff492b562a2d6f
FAMROGU\Paula <juan_esteban_199501@hotmail.com>
2025-02-18 16:38:40 -0500

    erhzgjngfjngj

---
+++ Agrolifenet.FrontEnd/Pages/Notificaciones.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Agrolifenet.FrontEnd.Pages
+{
+    public partial class Notificaciones : ComponentBase
+    {
+        private bool showNotification = false;
+
+        public void Show()
+        {
+            showNotification = true;
+            StateHasChanged();
+        }
+
+        public void Hide()
+        {
+            showNotification = false;
+            StateHasChanged();
+        }
+
+        private Notification notificationComponent;
+
+        private void ShowNotification()
+        {
+            notificationComponent.Show();
+        }
+    }
+}
